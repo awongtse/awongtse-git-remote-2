c5e1e4d621d9f2f89b5df58eb1fa445553e325c2
awongtse <awongtse87@gmail.com>
2024-05-21 07:08:32 +0700

    menambahkan file css

--- index.php
+++ index.php
@@ -8,7 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit, accusantium aperiam? Numquam officiis nulla, quasi earum totam quam magni minus ipsum quidem, reiciendis ipsa cupiditate facilis praesentium odit provident tenetur.</p>
-    <li>satu</li>
-    <li>dua</li>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
